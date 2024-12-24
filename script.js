2e3884e3379dfa66d11642c940f862b537de7467
Marco CARPENTIER <marcarp@protonmail.com>
2024-12-24 18:07:57 +0100

    Use of the length() method

--- script.js
+++ script.js
@@ -21,6 +21,9 @@ scene.add(mesh);
 // The mesh can be updated after it has been added to the scene but BEFORE the scene is rendered
 mesh.position.set(2,-1,-0.5);
 
+// Get the length of the Vector (distance between mesh position and origin (0,0,0))
+console.log(mesh.position.length());
+
 // Camera
 const camera = new THREE.PerspectiveCamera(75, size.width/size.heigh);
 camera.position.z = 3;
